cb804f63acffb34c0aa03e4070c0ef9555044a09
Lucas Perez Casal <luke@notedebian.fibertel.com.ar>
2026-04-11 01:13:01 -0300

    tp 2

---
+++ Tp2/Ejercicio5/src/main/java/Ejercicio5/Ejercicio5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Ejercicio5;
+
+/**
+ *
+ * @author luke
+ */
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
